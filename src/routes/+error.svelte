42170b0384f6426cc5f321ab761da71d5c37011e
Nils Wrenger <nils@wrenger.net>
2025-01-27 17:11:35 +0100

    :sparkles: Add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<div class="content">
+	<h2 class="h2">
+		<span class="text-glowing">{page.status} - {page.error?.message || 'Unknown Error'}</span>
+	</h2>
+</div>
